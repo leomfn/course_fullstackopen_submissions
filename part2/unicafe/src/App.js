9b297fa99b23f56b85a3689c541e255af7a014b2
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-01-11 16:10:28 +0100

    solution part1d step1

---
+++ part2/unicafe/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const Button = ({ text, eventHandler }) => {
+  return (
+    <button onClick={eventHandler}>
+      {text}
+    </button>
+  )
+}
+
+const Statistics = ({ props }) => {
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const giveFeedback = (rating) => () => {
+    if (rating === 'good') {
+      setGood(good + 1)
+    } else if (rating === 'neutral') {
+      setNeutral(neutral + 1)
+    } else {
+      setBad(bad + 1)
+    }
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text='good' eventHandler={giveFeedback('good')} />
+      <Button text='neutral' eventHandler={giveFeedback('neutral')} />
+      <Button text='bad' eventHandler={giveFeedback('bad')} />
+      <h1>statistics</h1>
+      <Statistics props={{good: good, neutral: neutral, bad: bad}} />
+    </div>
+  )
+}
+
+export default App
