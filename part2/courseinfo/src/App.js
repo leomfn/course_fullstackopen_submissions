badc51b9938596b4b7262d1a3ba0072a18a6ee97
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-01-12 00:42:25 +0100

    separated Course module with subcomponents

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,29 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ sum }) => <p><strong>total of {sum} exercises</strong></p>
-
-const Content = ({ parts }) =>
-    <>
-        {parts.map(
-            (part) => <Part key={part.id} part={part} />
-        )}
-    </>
-
-const Part = ({ part }) =>
-    <p>
-        {part.name} {part.exercises}
-    </p>
-
-const Course = ({ course }) => {
-    const { name, parts } = course
-    return (
-        <>
-            <Header course={name} />
-            <Content parts={parts} />
-            <Total sum={parts.reduce((sum, part) => sum + part.exercises, 0)} />
-        </>
-    )
-}
+import Course from "./components/Course"
 
 const App = () => {
     const courses = [
