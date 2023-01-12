1d6c1c2c8960a9f85e0a8aa3a0af2f0d7afc0958
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-01-12 20:59:02 +0100

    solution phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -11,10 +11,14 @@ const AddressList = (props) => {
 const App = () => {
   // state components
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterString, setFilterString] = useState('')
 
   // event handler functions
   const nameInputHandler = (event) => {
@@ -39,9 +43,22 @@ const App = () => {
     setNewNumber('')
   }
 
+  const filterPhonebookHandler = (event) => {
+    setFilterString(event.target.value)
+  }
+
+  // other
+  const personsFiltered = filterString === ''
+    ? [...persons]
+    : persons.filter((person) => person.name.toLowerCase().includes(filterString.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={filterPhonebookHandler} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={submitHandler}>
         <div>
           name: <input onChange={nameInputHandler} value={newName} />
@@ -55,7 +72,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => AddressList(person))}
+        {personsFiltered.map(person => AddressList(person))}
       </div>
     </div>
   )
