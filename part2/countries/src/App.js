9da96b926b3cfb81af4914829b00c9003b4c561f
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-01-17 17:20:28 +0100

    solution countries step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,95 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 
+const Weather = ({ city, lat, lon }) => {
+  // state hooks
+  // const [weather, setWeather] = useState(
+  //   {
+  //     "coord": {
+  //       "lon": -0.13,
+  //       "lat": 51.51
+  //     },
+  //     "weather": [
+  //       {
+  //         "id": 300,
+  //         "main": "Drizzle",
+  //         "description": "light intensity drizzle",
+  //         "icon": "09d"
+  //       }
+  //     ],
+  //     "base": "stations",
+  //     "main": {
+  //       "temp": 280.32,
+  //       "pressure": 1012,
+  //       "humidity": 81,
+  //       "temp_min": 279.15,
+  //       "temp_max": 281.15
+  //     },
+  //     "visibility": 10000,
+  //     "wind": {
+  //       "speed": 4.1,
+  //       "deg": 80
+  //     },
+  //     "clouds": {
+  //       "all": 90
+  //     },
+  //     "dt": 1485789600,
+  //     "sys": {
+  //       "type": 1,
+  //       "id": 5091,
+  //       "message": 0.0103,
+  //       "country": "GB",
+  //       "sunrise": 1485762037,
+  //       "sunset": 1485794875
+  //     },
+  //     "id": 2643743,
+  //     "name": "London",
+  //     "cod": 200
+  //   })
+
+  const [weather, setWeather] = useState({})
+  const [weatherAPIStatus, setWeatherAPIStatus] = useState(false)
+
+
+
+  // event hooks
+  useEffect(
+    () => {
+      const weatherAPI = process.env.REACT_APP_WEATHER_API_KEY
+      const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherAPI}`
+      axios
+        .get(weatherURL)
+        .then(
+          response => {
+            console.log('status weather api', response.status)
+            setWeatherAPIStatus(response.status === 200)
+            setWeather(response.data)
+          }
+        )
+        // eslint-disable-next-line
+    }, []
+  )
+
+  if (weatherAPIStatus) {
+    return (
+      <div>
+        <h2>Weather in {city}</h2>
+        <div>temperature {Math.round(weather.main.temp - 273.15)} °Celsius</div>
+        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt='weather icon' />
+        <div>wind {weather.wind.speed} m/s</div>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>Weather in {city}</h2>
+        <div>No data available</div>
+      </div>
+    )
+  }
+
+}
+
 const CountryInfo = ({ country }) => {
   return (
     <div>
@@ -18,9 +107,9 @@ const CountryInfo = ({ country }) => {
             <li key={language}>{language}</li>
           )
         })}
-
       </ul>
-      <img src={country.flags.svg} alt='country flag' height='200px' />
+      <img src={country.flags.svg} alt='country flag' height='150x' />
+      <Weather city={country.capital[0]} lat={country.capitalInfo.latlng[0]} lon={country.capitalInfo.latlng[1]} />
     </div>
   )
 }
@@ -54,7 +143,7 @@ const Countries = ({ countryArray }) => {
       <div>
         {countryArray.map((country) => {
           return (
-            <CountrySelection country={country} />
+            <CountrySelection key={country.name.common} country={country} />
           )
         })}
       </div>
