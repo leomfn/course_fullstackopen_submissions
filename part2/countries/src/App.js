8f0bad35ecea34dbe01bfe0ae42858f0c8c2dc75
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-01-17 17:23:26 +0100

    typos

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,56 +3,10 @@ import axios from "axios"
 
 const Weather = ({ city, lat, lon }) => {
   // state hooks
-  // const [weather, setWeather] = useState(
-  //   {
-  //     "coord": {
-  //       "lon": -0.13,
-  //       "lat": 51.51
-  //     },
-  //     "weather": [
-  //       {
-  //         "id": 300,
-  //         "main": "Drizzle",
-  //         "description": "light intensity drizzle",
-  //         "icon": "09d"
-  //       }
-  //     ],
-  //     "base": "stations",
-  //     "main": {
-  //       "temp": 280.32,
-  //       "pressure": 1012,
-  //       "humidity": 81,
-  //       "temp_min": 279.15,
-  //       "temp_max": 281.15
-  //     },
-  //     "visibility": 10000,
-  //     "wind": {
-  //       "speed": 4.1,
-  //       "deg": 80
-  //     },
-  //     "clouds": {
-  //       "all": 90
-  //     },
-  //     "dt": 1485789600,
-  //     "sys": {
-  //       "type": 1,
-  //       "id": 5091,
-  //       "message": 0.0103,
-  //       "country": "GB",
-  //       "sunrise": 1485762037,
-  //       "sunset": 1485794875
-  //     },
-  //     "id": 2643743,
-  //     "name": "London",
-  //     "cod": 200
-  //   })
-
   const [weather, setWeather] = useState({})
   const [weatherAPIStatus, setWeatherAPIStatus] = useState(false)
 
-
-
-  // event hooks
+  // effect hooks
   useEffect(
     () => {
       const weatherAPI = process.env.REACT_APP_WEATHER_API_KEY
@@ -163,7 +117,7 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [displayCountries, setDisplayCountries] = useState([])
 
-  // event hooks
+  // effect hooks
   useEffect(
     () => {
       axios
