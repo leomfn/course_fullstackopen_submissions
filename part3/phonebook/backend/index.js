30fde17e564eb079f464a085e237edc487a1c024
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-01-22 22:17:01 +0100

    solution phonebook database step2

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -72,23 +72,16 @@ app.get('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name) {
-    return response.status(400).json({ error: 'name missing' })
-  } else if (!body.number) {
-    return response.status(400).json({ error: 'number missing' })
-  } else if (persons.map(p => p.name).includes(body.name)) {
-    return response.status(400).json({ error: 'name must be unique' })
-  }
-
-  const person = {
-    id: Math.ceil(Math.random() * 1000),
+  const person = new Person({
     name: body.name,
     number: body.number
-  }
+  })
 
-  persons.push(person)
-  response.json(person)
-
+  person
+    .save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
--- part3/phonebook/frontend/src/components/PersonForm.js
+++ part3/phonebook/frontend/src/components/PersonForm.js
@@ -9,40 +9,40 @@ const PersonForm = ({
 
     const submitHandler = (event) => {
         event.preventDefault()
-        if (persons.map(person => person.name).includes(newName)) {
+        // if (persons.map(person => person.name).includes(newName)) {
 
-            const updatePerson = { ...persons.find((p) => p.name === newName), number: newNumber }
-            if (window.confirm(`${updatePerson.name} is already added to phonebook, replace the old number with a new one?`)) {
-                personService
-                    .update(updatePerson.id, updatePerson)
-                    .then(setPersons(persons.map((p => p.id !== updatePerson.id ? p : updatePerson))))
-                    .catch(error => {
-                        setErrorMessage(`Information of ${newName} has already been removed from server`)
-                        setTimeout(() => {
-                            setErrorMessage(null)
-                        }, 5000)
-                        setPersons(persons.filter(p => p.id !== updatePerson.id))
-                    })
-                
-            }
-        } else {
-            const newPerson = {
-                name: newName,
-                number: newNumber
-            }
-            personService
-                .create(newPerson)
-                .then(
-                    returnedEntry => {
-                        setPersons(persons.concat(returnedEntry))
-                        setNotificationMessage(
-                            `Added ${newName}`
-                        )
-                        setTimeout(() => {
-                            setNotificationMessage(null)
-                        }, 5000)
-                    })
+        //     const updatePerson = { ...persons.find((p) => p.name === newName), number: newNumber }
+        //     if (window.confirm(`${updatePerson.name} is already added to phonebook, replace the old number with a new one?`)) {
+        //         personService
+        //             .update(updatePerson.id, updatePerson)
+        //             .then(setPersons(persons.map((p => p.id !== updatePerson.id ? p : updatePerson))))
+        //             .catch(error => {
+        //                 setErrorMessage(`Information of ${newName} has already been removed from server`)
+        //                 setTimeout(() => {
+        //                     setErrorMessage(null)
+        //                 }, 5000)
+        //                 setPersons(persons.filter(p => p.id !== updatePerson.id))
+        //             })
+
+        //     }
+        // } else {
+        const newPerson = {
+            name: newName,
+            number: newNumber
         }
+        personService
+            .create(newPerson)
+            .then(
+                returnedEntry => {
+                    setPersons(persons.concat(returnedEntry))
+                    setNotificationMessage(
+                        `Added ${newName}`
+                    )
+                    setTimeout(() => {
+                        setNotificationMessage(null)
+                    }, 5000)
+                })
+        // }
         setNewName('')
         setNewNumber('')
     }
