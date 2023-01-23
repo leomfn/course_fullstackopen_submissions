848b79d2dc378fc4299d1bd35a25a839a0b31532
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-01-23 14:59:08 +0100

    eslint fixes

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -29,29 +29,6 @@ const customMorgan = morgan((tokens, req, res) => {
 
 app.use(customMorgan)
 
-// let persons = [
-//   {
-//     "id": 1,
-//     "name": "Arto Hellas",
-//     "number": "040-123456"
-//   },
-//   {
-//     "id": 2,
-//     "name": "Ada Lovelace",
-//     "number": "39-44-5323523"
-//   },
-//   {
-//     "id": 3,
-//     "name": "Dan Abramov",
-//     "number": "12-43-234345"
-//   },
-//   {
-//     "id": 4,
-//     "name": "Mary Poppendieck",
-//     "number": "39-23-6423122"
-//   }
-// ]
-
 app.get('/api/persons', (request, response) => {
   Person
     .find({})
@@ -100,7 +77,7 @@ app.post('/api/persons', (request, response, next) => {
 app.delete('/api/persons/:id', (request, response, next) => {
   Person
     .findByIdAndRemove(request.params.id)
-    .then(result => response.status(204).end())
+    .then(() => response.status(204).end())
     .catch(error => next(error))
 })
 
--- part3/phonebook/backend/models/person.js
+++ part3/phonebook/backend/models/person.js
@@ -7,40 +7,40 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose
-    .connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error)
-    })
+  .connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: v => {
-                return /\d{2,3}-\d+/.test(v)
-            },
-            message: props => {
-                return `${props.value} is not a valid phone number!`
-            }
-        }
+  name: {
+    type: String,
+    minLength: 3
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: v => {
+        return /\d{2,3}-\d+/.test(v)
+      },
+      message: props => {
+        return `${props.value} is not a valid phone number!`
+      }
     }
+  }
 })
 
 // id as string and delete default _id and __v fields
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
