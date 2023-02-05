9a8112aae8f8239d1f80d719171334d3cad5d982
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-02-05 14:45:11 +0100

    solution bloglist expansion step3

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 mongoose.set('strictQuery', false)
 
@@ -19,5 +20,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
