3aa4374c1413392cf5d4c2dbece2d5413566536a
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-02-13 19:19:37 +0100

    solution blog list tests step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
-// import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('By default, blog title and author are rendered, but not url', () => {
+describe('<Blog /> component', () => {
   const blog = {
     title: 'Test title',
     author: 'test author',
     url: 'testurl.com',
-    likes: 100
+    likes: 100,
+    user: {
+      username: 'test'
+    }
   }
 
   const user = {
@@ -20,25 +23,47 @@ test('By default, blog title and author are rendered, but not url', () => {
 
   const setBlogs = jest.fn()
 
-  const { container } = render(
-    <Blog blog={blog} token={token} user={user} setBlogs={setBlogs} />
-  )
+  let container
 
-  const div = container.querySelector('.blog')
+  beforeEach(() => {
 
-  expect(div).toHaveTextContent(
-    blog.title
-  )
+    container = render(
+      <Blog blog={blog} token={token} user={user} setBlogs={setBlogs} />
+    ).container
+  })
 
-  expect(div).toHaveTextContent(
-    blog.author
-  )
+  test('By default, blog title and author are rendered, but not url', () => {
+    const div = container.querySelector('.blog')
 
-  expect(div).not.toHaveTextContent(
-    blog.url
-  )
+    expect(div).toHaveTextContent(
+      blog.title
+    )
 
-  expect(div).not.toHaveTextContent(
-    blog.likes
-  )
+    expect(div).toHaveTextContent(
+      blog.author
+    )
+
+    expect(div).not.toHaveTextContent(
+      blog.url
+    )
+
+    expect(div).not.toHaveTextContent(
+      blog.likes
+    )
+  })
+
+  test.only('URL and number of likes are shown when button is clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+    const div = container.querySelector('.blog')
+
+    expect(div).toHaveTextContent(
+      blog.url
+    )
+
+    expect(div).toHaveTextContent(
+      blog.likes
+    )
+  })
 })
